98bb69bc1bc2cfe73f4858e176ad8180a208dfdf
Luis Galeas <luis.galeasjr@gmail.com>
2023-11-14 04:23:49 -0500

    Update Portfolio outline

--- src/core/components/Portfolio/Portfolio.jsx
+++ src/core/components/Portfolio/Portfolio.jsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
+import "./portfolio.scss";
 
 const Portfolio = () => {
     return (
         <React.Fragment>
-            <section id='portfolio' className='test-div'>
-                    <h2>Portfolio Component</h2>
+            <section id='portfolio' className='portfolio'>
+                    <div className='portfolio__item'>
+                        <div className='item__section'>
+                            <img />
+                            <button></button>
+                        </div>
+                        <p> This is a placeholder</p>
+                    </div>
             </section>
         </React.Fragment>
     )
