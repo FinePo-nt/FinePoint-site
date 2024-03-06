1ba51310fb35528d4891bc1854bb6fdae495b4ce
Lucy Yang <40499685+l-yang-05@users.noreply.github.com>
2024-03-06 16:27:03 -0500

    Adding in changes for nav bar and updated faq component

--- src/core/components/FAQ/FAQ.jsx
+++ src/core/components/FAQ/FAQ.jsx
@@ -1,11 +1,44 @@
 import * as React from 'react';
-import Contact from '../Contact/Contact'
+import Contact from '../Contact/Contact';
+import Accordion from '@mui/material/Accordion';
+import AccordionActions from '@mui/material/AccordionActions';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Button from '@mui/material/Button';
 
 const FAQ = () => {
     return (
         <React.Fragment>
-            <div className='test-div'>
-                <h2>FAQ Page</h2>
+            <div className='faq-container' id='FAQ'>
+                <h2 className='contact-h2'>Frequently Asked Questions</h2>
+                <Accordion>
+                    <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1-content" id="panel1-header">
+                        Q1. Lorem ipsum dolor sit amet, in lobortis lorem accumsan?
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                        malesuada lacus ex, sit amet blandit leo lobortis eget.
+                    </AccordionDetails>
+                </Accordion>
+                <Accordion>
+                    <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1-content" id="panel1-header">
+                        Q2. Lorem ipsum dolor sit amet, in lobortis lorem accumsan?
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                        malesuada lacus ex, sit amet blandit leo lobortis eget.
+                    </AccordionDetails>
+                </Accordion>
+                <Accordion>
+                    <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1-content" id="panel1-header">
+                        Q3. Lorem ipsum dolor sit amet, in lobortis lorem accumsan?
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                        malesuada lacus ex, sit amet blandit leo lobortis eget.
+                    </AccordionDetails>
+                </Accordion>
             </div>
             <Contact/>
         </React.Fragment>
